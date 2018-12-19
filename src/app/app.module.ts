2b44b3aa52da9a668fbb7fd8ff3026449f86b9d6
Nattalie Zapien <nattaliez@mail.fresnostate.edu>
2018-12-19 00:41:33 -0800

    LabAssignment7 finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {ToastModule} from './toast/toast.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +10,6 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
